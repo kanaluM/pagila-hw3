00992a75df83851e96a9c9f634946c244b2da882
kanaluM <kmonaco@g.hmc.edu>
2023-03-01 17:32:22 -0800

    finished first 4 problems roughly

--- sql/02.sql
+++ sql/02.sql
@@ -1,3 +1,13 @@
 /*
  * Compute the country with the most customers in it. 
  */
+
+SELECT country FROM
+(SELECT country, count(customer_id)
+FROM country
+JOIN city USING (country_id)
+JOIN address USING (city_id)
+JOIN customer USING (address_id)
+GROUP BY country
+ORDER BY count DESC) AS t
+LIMIT 1;
